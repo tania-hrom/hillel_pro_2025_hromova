54071070190a9963aa1d68be6064e21cfeb90d41
tetiana-hromova <hromova.tetiana@airslate.com>
2024-12-22 14:42:23 +0200

    First commit

---
+++ hillel_pro_2025/src/main/java/lesson_1/Main.java
@@ -0,0 +1,18 @@
+package lesson_1;
+
+public class Main {
+
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter Application");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Result is " + pounds + " pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+}
