74c10d8c4cc3e91126fc524ce2844d19874930bf
tania-hrom <tania.gromovaa@gmail.com>
2025-01-16 10:57:39 +0200

    Lesson 7 commit

---
+++ hillel_2025/src/main/java/lesson_7/Corrector.java
@@ -0,0 +1,17 @@
+package lesson_7;
+
+public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+        for (String str : strs) {
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+            count++;
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ hillel_2025/src/main/java/lesson_7/DataProvider.java
@@ -0,0 +1,8 @@
+package lesson_7;
+
+public class DataProvider {
+
+    public String[] getData() {
+        return new String[]{"brange", "plum", "tomato", "onibn", "grape"};
+    }
+}
